ff08a86c7edae0f2debc109c95559afc2d2e63b0
XiaorongPeng <1572893675@qq.com>
2022-08-01 01:38:48 +0800

    to read the number of different samples

---
+++ Xiaorong Peng/src/firstVersion/testMain.java
@@ -0,0 +1,25 @@
+package firstVersion;
+
+import java.util.Scanner;
+
+public class testMain {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("the number of laptop: ");
+		int numLaptop = sc.nextInt();
+		System.out.print("the number of laptop: ");
+		int numMouse = sc.nextInt();
+		System.out.print("the number of laptop: ");
+		int numDesktop = sc.nextInt();
+		System.out.print("the number of laptop: ");
+		int numLCD = sc.nextInt();
+		sc.close();
+		
+
+	}
+	
+	
+
+}
