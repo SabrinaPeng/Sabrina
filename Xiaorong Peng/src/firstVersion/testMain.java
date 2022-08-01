2cfd6b9cb829345c76dac5dc269ce3cb00e805e9
XiaorongPeng <1572893675@qq.com>
2022-08-01 18:07:24 +0800

    newest version

--- Xiaorong Peng/src/firstVersion/testMain.java
+++ Xiaorong Peng/src/firstVersion/testMain.java
@@ -24,6 +24,7 @@ public class testMain {
 		double totalVol = numLaptop * 0.6 * 0.5 * 0.5 + numMouse * 0.3 * 0.3 * 0.2 + 
 				numDesktop * 1 * 1.5 * 0.5 + numLCD * 1.2 * 1.4 * 0.8;
 		double totalWt = numLaptop * 6.5 + numMouse * 0.2 + numDesktop * 20 + numLCD * 2.6;
+		double den = totalWt / totalVol;
 		double volSmallContainer = 2.59 * 2.43 * 6.06;
 		double volBigContainer = 2.59 * 2.43 * 12.01;
 		System.out.println("\nThe total volume of the shipment: " + totalVol + " m^3");
@@ -32,30 +33,34 @@ public class testMain {
 		System.out.println("The volume of the big container: " + volBigContainer + " m^3\n");
 		
 		//to select the type of container and compare price methods
-		int cost = 0;
-		int numBig = (int) (totalVol / volBigContainer);
-		int numSmall = 0;
-		if (totalVol % volBigContainer <= volSmallContainer) {
-			numSmall++;
-		}else {
-			numBig++;
-		}
-		cost += numBig * 1800;
-		if (numSmall > 0) {
-			double den = totalWt / totalVol;
-			double x = (totalVol % volBigContainer) * den;
-			if (x > 500) {
-				cost += 1200;
-			}else {
-				cost += 1000;
+		int numBig = (int) (Math.ceil(totalVol / volBigContainer));
+		int numSmall = 0, numSmallTemp = 0;
+		int price = numBig * 1800;
+		int newPrice = price;
+		while(newPrice <= price) {
+			numSmallTemp = numSmall;
+			price = newPrice;
+			int priceSmall;
+			numBig--;
+			if(numBig < 0) {
+				break;
 			}
+			double remainVol = totalVol - volBigContainer * numBig;
+			numSmall = (int) (Math.ceil(remainVol / volSmallContainer));
+			//judge the price of small container
+			if (den * remainVol / numSmall > 500)
+				priceSmall = 1200;
+			else
+				priceSmall = 1000;
+			newPrice = numBig * 1800 + numSmall * priceSmall;
 		}
+		numBig++;
 		
 		//print the results
 		System.out.println("------------------Result-------------------");
-		System.out.println("The optimal solution is " + cost + " Euro");
+		System.out.println("The optimal solution is " + price + " Euro");
 		System.out.println("The number of big container: " + numBig);
-		System.out.println("The number of small container: " + numSmall);
+		System.out.println("The number of small container: " + numSmallTemp);
 		
 
 	}
