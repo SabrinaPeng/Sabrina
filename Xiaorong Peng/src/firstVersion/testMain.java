25c02e6bfd410051d63796fb1bf39e27debd4200
XiaorongPeng <1572893675@qq.com>
2022-08-01 15:35:00 +0800

    draft

--- Xiaorong Peng/src/firstVersion/testMain.java
+++ Xiaorong Peng/src/firstVersion/testMain.java
@@ -2,25 +2,62 @@ package firstVersion;
 
 import java.util.Scanner;
 
-
 public class testMain {
 
 	public static void main(String[] args) {
 
+		//to read the order
 		Scanner sc = new Scanner(System.in);
 		System.out.print("the number of laptop: ");
 		int numLaptop = sc.nextInt();
-		System.out.print("the number of laptop: ");
+		System.out.print("the number of mouse: ");
 		int numMouse = sc.nextInt();
-		System.out.print("the number of laptop: ");
+		System.out.print("the number of desktop: ");
 		int numDesktop = sc.nextInt();
-		System.out.print("the number of laptop: ");
+		System.out.print("the number of LCDscreen: ");
 		int numLCD = sc.nextInt();
 		sc.close();
 		
+		//to save the order in arrays
+		int[] list = {numLaptop, numMouse, numDesktop, numLCD};
+		//to compute the total volume and weight of the shipment and the total volume of both containers
+		double totalVol = numLaptop * 0.6 * 0.5 * 0.5 + numMouse * 0.3 * 0.3 * 0.2 + 
+				numDesktop * 1 * 1.5 * 0.5 + numLCD * 1.2 * 1.4 * 0.8;
+		double totalWt = numLaptop * 6.5 + numMouse * 0.2 + numDesktop * 20 + numLCD * 2.6;
+		double volSmallContainer = 2.59 * 2.43 * 6.06;
+		double volBigContainer = 2.59 * 2.43 * 12.01;
+		System.out.println("\nThe total volume of the shipment: " + totalVol + " m^3");
+		System.out.println("The total weight of the shipment: " + totalWt + " kg\n");
+		System.out.println("The volume of the small container: " + volSmallContainer + " m^3");
+		System.out.println("The volume of the big container: " + volBigContainer + " m^3\n");
+		
+		//to select the type of container and compare price methods
+		int cost = 0;
+		int numBig = (int) (totalVol / volBigContainer);
+		int numSmall = 0;
+		if (totalVol % volBigContainer <= volSmallContainer) {
+			numSmall++;
+		}else {
+			numBig++;
+		}
+		cost += numBig * 1800;
+		if (numSmall > 0) {
+			double den = totalWt / totalVol;
+			double x = (totalVol % volBigContainer) * den;
+			if (x > 500) {
+				cost += 1200;
+			}else {
+				cost += 1000;
+			}
+		}
+		
+		//print the results
+		System.out.println("------------------Result-------------------");
+		System.out.println("The optimal solution is " + cost + " Euro");
+		System.out.println("The number of big container: " + numBig);
+		System.out.println("The number of small container: " + numSmall);
+		
 
 	}
-	
-	
 
 }
